3d6660d8332645a593fdf57f44314cb9d5f7410a
Alexander Higgins <alexhiggins732@hotmail.com>
2021-02-10 14:49:02 -0500

    Ma Plot Updates

--- MaUtil/Program.cs
+++ MaUtil/Program.cs
@@ -51,14 +51,21 @@ namespace MaUtil
 
         public decimal PortfolioNetProfit => PortfolioValue / 1000m - 1;
 
+        public int AgentIndex { get; internal set; }
+        public int K { get; internal set; }
+        public int I { get; internal set; }
+
         public decimal Commission;
 
         private decimal latestClose;
         public int ShortPeriod;
         public int LongPeriod;
-
+        //GoldTrade lastTrade = new GoldTrade();
+        //int TradeCount;
         public void SetGold(DateTime dt, decimal close, bool isGold)
         {
+            //lock (lastTrade)
+            //{
             latestClose = close;
             if (isGold != IsGold)
             {
@@ -73,13 +80,22 @@ namespace MaUtil
                         var coinQty = qty / close;
                         var trade = new GoldTrade { BuyDate = dt, BuyPrice = close, BuyQty = coinQty, BuyValue = qty, Commission = commisionAmount };
                         Trades.Add(trade);
+                        //lastTrade = new GoldTrade { BuyDate = dt, BuyPrice = close, BuyQty = coinQty, BuyValue = qty, Commission = commisionAmount };
+                        //lastTrade.BuyDate = dt;
+                        //lastTrade.BuyPrice = close;
+                        //lastTrade.BuyQty = coinQty;
+                        //lastTrade.BuyValue = qty;
+                        //lastTrade.Commission = commisionAmount;
+                        //TradeCount += 1;
                         this.CoinQty = coinQty;
                         this.UsdQty = 0;
+
                     }
                 }
                 else
                 {
                     holding = false;
+                    //var trade = lastTrade;
                     var trade = Trades.Last();
                     trade.SellDate = dt;
                     trade.SellPrice = close;
@@ -92,10 +108,13 @@ namespace MaUtil
                 }
             }
             IsGold = isGold;
+            //}
         }
         public override string ToString()
         {
+            //return $"{Name} - {(IsGold ? "(Gold)" : "(Dark)")} {PortfolioValue.ToString("C2")} ({PortfolioNetProfit.ToString("P2")}) - {TradeCount.ToString("N0")} Trades ";
             return $"{Name} - {(IsGold ? "(Gold)" : "(Dark)")} {PortfolioValue.ToString("C2")} ({PortfolioNetProfit.ToString("P2")}) - {Trades.Count.ToString("N0")} Trades ";
+
         }
         public static GoldAgent[] CreateAgents(int maxMa, decimal commission = 0)
         {
@@ -121,7 +140,7 @@ namespace MaUtil
         static ProductType SelectedProductType = ProductType.LtcUsd;
         static void Main(string[] args)
         {
-            var startDate = DateTime.Parse("1/1/2019");
+            var startDate = DateTime.Parse("1/1/2020");
 
             ProductType productType = ParseProductType();
 
@@ -159,6 +178,7 @@ namespace MaUtil
         }
         static void TestMaStrategy(CandleGranularity granularity, DateTime? startDate = null, int maxMa = 256, ProductType productType = ProductType.LtcUsd)
         {
+            CandleService.UpdateCandles(productType, true);
             DateTime readerStart = DateTime.MinValue;
             if (startDate == null)
             {
@@ -168,18 +188,48 @@ namespace MaUtil
             {
                 readerStart = CandleService.GetMinDbCandleDateForSampleSizeAndStartDate(productType, granularity, startDate.Value, maxMa);
             }
+
             var reader = new MaReader(maxMa, productType, granularity, readerStart);
 
             bool stepped = reader.Step();
 
             var agents = GoldAgent.CreateAgents(maxMa, 0.0025m);
+            int y = 0;
+            for (var i = 0; i < reader.MaxMa; i++)
+            {
+                //Console.Title = $"Calculating {i} of {m.Length}";
+                for (var k = 0; k < reader.MaxMa; k++)
+                {
+                    y = i * reader.MaxMa + k;
+                    agents[y].AgentIndex = y;
+                    agents[y].K = k;
+                    agents[y].I = i;
+
+
+                }
+            }
+
             bool isGold;
             int x = 0;
-            int y = 0;
+            y = 0;
+            var swStep = new System.Diagnostics.Stopwatch();
+            var swMatrix = new System.Diagnostics.Stopwatch();
+            var swGold = new System.Diagnostics.Stopwatch();
+            ParallelOptions parallelOptions = new ParallelOptions() { MaxDegreeOfParallelism = 16 };
+            DateTime CurrentTime= DateTime.MinValue;
             while (stepped)
             {
+
+                CurrentTime = reader.Current.Time;
                 if (reader.Current.Time.Day == 1 && reader.Current.Time.Hour == 0 && reader.Current.Time.Minute == 0)
+                {
                     Console.WriteLine($"[{DateTime.Now}] - {reader.Current.Time.Date}");
+                    Console.WriteLine($" => Step:  {swStep.Elapsed}  => Matrix: {swMatrix.Elapsed}  Gold=> {swGold.Elapsed}");
+                    swStep = new System.Diagnostics.Stopwatch();
+                    swMatrix = new System.Diagnostics.Stopwatch();
+                    swGold = new System.Diagnostics.Stopwatch();
+                }
+                swStep.Start();
                 if (startDate != null)
                 {
                     if (reader.Current.Time < startDate.Value)
@@ -189,9 +239,32 @@ namespace MaUtil
                         continue;
                     }
                 }
+                swStep.Stop();
+
+                swGold.Start();
                 var m = reader.Matrix;
+                swGold.Stop();
+
+                swMatrix.Start();
+                //Parallel.For(0, m.Length, parallelOptions, (i) =>
+                //{
+                //    for (var i = 0; i < m.Length; i++)
+                //    {
+                //        Console.Title = $"Calculating {i} of {m.Length}";
+                //        var row = m[i];
+                //        var offset = i * reader.MaxMa;
+                //        for (var k = 0; k < reader.MaxMa; k++)
+                //        {
+                //            y = offset + k;
+                //            isGold = row[k];
+                //            agents[y].SetGold(reader.Current.Time, reader.Current.Close.Value, isGold);
+                //        }
+                //    }
+                //});
+
                 for (var i = 0; i < m.Length; i++)
                 {
+                    //Console.Title = $"Calculating {i} of {m.Length}";
                     for (var k = 0; k < reader.MaxMa; k++)
                     {
                         y = i * reader.MaxMa + k;
@@ -199,16 +272,21 @@ namespace MaUtil
                         agents[y].SetGold(reader.Current.Time, reader.Current.Close.Value, isGold);
                     }
                 }
+                swMatrix.Stop();
                 x++;
+                swMatrix.Start();
                 stepped = reader.Step();
+                swStep.Stop();
             }
 
-            var ts = DateTime.Now.ToString("yyyyMMdd_mmss");
+            var ts = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+            string fileName = $"{productType}-{granularity}-MaAgent";
             var sortedAgents = agents.OrderByDescending(a => a.PortfolioValue).ToArray();
+            CreateTrainingData(sortedAgents, maxMa, productType, granularity, readerStart, startDate.Value, CurrentTime, fileName, ts);
             var sortedSummaryLines = sortedAgents.Select(a => a.ToString()).ToArray();
             var sortedSummary = string.Join("\r\n", sortedSummaryLines);
 
-            string fileName = $"{productType}-{granularity}-MaAgent";
+
 
             if (startDate != null)
             {
@@ -239,11 +317,80 @@ namespace MaUtil
                 goldSurfaces[agentX - 1][agentY - 1] = agent.IsGold ? 1 : 0;
             }
             var json = JsonConvert.SerializeObject(surfaces);
-            File.WriteAllText(fileName + ts + ".json", $"var z_data={json};");
+            var plotJson = fileName + ts + ".json";
+            File.WriteAllText(plotJson, $"var z_data={json};");
             json = JsonConvert.SerializeObject(goldSurfaces);
-            File.WriteAllText(fileName + ts + "-gold.json", $"var z_data={json};");
+            var plotGoldJson = fileName + ts + "-gold.json";
+            File.WriteAllText(plotGoldJson, $"var z_data={json};");
+
+            SetSrc(plotJson, "plot.html");
+            SetSrc(plotGoldJson, "plot-gold.html");
 
         }
+
+        private static void CreateTrainingData(
+            GoldAgent[] sortedAgents,
+            int maxMa,
+            ProductType productType,
+            CandleGranularity granularity,
+            DateTime readStartDate,
+            DateTime maStartDate,
+            DateTime endDate,
+            string fileName,
+            string timeStamp)
+        {
+            var f = sortedAgents.First();
+            var reader = new MaReader(maxMa, productType, granularity, readStartDate);
+            var stepped = reader.Step();
+            while (reader.Current.Time < maStartDate)
+            {
+                stepped = reader.Step();
+            }
+
+            bool[][] m;
+            bool isGold;
+            var i = f.I;
+            var k = f.K;
+            Console.WriteLine($"[{DateTime.Now}] - Creating Training data for {f}");
+            var dataFolder = Directory.CreateDirectory( "train-" + fileName + timeStamp );
+            var tsvFile = Path.Combine(dataFolder.FullName, "training-data.txt");
+            int x = 0;
+            using (var sw = new StreamWriter(tsvFile))
+            {
+                sw.WriteLine($"i\time\tside\timage");
+                while (stepped)
+                {
+                    if (reader.Current.Time.Day == 1 && reader.Current.Time.Hour == 0 && reader.Current.Time.Minute == 0)
+                    {
+                        Console.WriteLine($"[{DateTime.Now}] - {reader.Current.Time.Date}");
+                    }
+                    m = reader.Matrix;
+                    isGold = m[i][k];
+              
+                    var bmp = reader.Bitmap;
+                    var candleStamp = reader.Current.Time.ToString("yyyyMMdd_HHmm");
+                    var imageName = fileName + timeStamp + $"-{candleStamp}.bmp";
+                    var imagePath = Path.Combine(dataFolder.FullName, imageName);
+                    bmp.Save(imagePath, System.Drawing.Imaging.ImageFormat.Bmp);
+                    var lineData = $"{++x}\t{reader.Current.Time}\t{(isGold ? 1.0 : 0.0)}\t{imagePath}";
+                    sw.WriteLine(lineData);
+                    stepped = reader.Step();
+
+                }
+
+            }
+
+        }
+
+        private static void SetSrc(string plotFile, string dest)
+        {
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            doc.Load(dest);
+            var el = doc.GetElementbyId("plotsrc");
+            el.SetAttributeValue("src", plotFile);
+            doc.Save(dest);
+        }
+
         static void MaWithBitmap()
         {
             var reader = new MaReader(256, ProductType.LtcUsd, CandleGranularity.Hour24);
